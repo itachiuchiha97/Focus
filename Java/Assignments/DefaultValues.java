1de12923ba61eddf6c9f99495da5b6fa0c519661
Sahil <sahil.singh.bhatia@gmail.com>
2018-09-30 11:35:39 +0530

    java learning

---
+++ Java/Assignments/DefaultValues.java
@@ -0,0 +1,13 @@
+
+class DefaultValues {
+    public static void main(String[] args) {
+        float pif = 22 / 7;
+        double pid = 22 / 7;
+        long pil = 22 / 7;
+
+        System.out.println(pif);
+        System.out.println(pid);
+        System.out.println(pil);
+
+    }
+}
