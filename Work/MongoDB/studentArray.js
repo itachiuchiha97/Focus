ae2939f1deb0c118ce556ec18b78c034bdeb86f4
Sahil <sahil.singh.bhatia@gmail.com>
2018-10-09 17:59:19 +0530

    mongoDB

---
+++ Work/MongoDB/studentArray.js
@@ -0,0 +1,19 @@
+var studentArray = [{
+    name: "Tushar",
+    class: "12th",
+    roll: 25
+},
+
+{
+    name: "Sahil",
+    class: "12th",
+    roll: 69
+},
+
+{
+    name: "Charul",
+    class: "9th",
+    roll: 23
+}
+
+]
---
+++ Work/MongoDB/studentInfo.js
@@ -0,0 +1,29 @@
+db.student.insert({
+    name: "Tushar",
+    class: "12th",
+    roll: 25
+})
+
+db.student.insert({
+    name: "Sahil",
+    class: "12th",
+    roll: 69
+})
+
+db.student.insert({
+    name: "Aruna",
+    class: "5th",
+    roll: 89
+})
+
+db.student.insert({
+    name: "Deepak",
+    class: "7th",
+    roll: 21
+})
+
+db.student.insert({
+    name: "Charul",
+    class: "9th",
+    roll: 23
+})
